15e84325c56726bf222c86852835403a760e4c8b
Jacob <encody@users.noreply.github.com>
2022-02-24 13:16:53 -0600

    hash artifact from circleci

--- web-rs/src/circleci_artifacts.rs
+++ web-rs/src/circleci_artifacts.rs
@@ -1,158 +1,129 @@
+use code_hash::CodeHash;
 use std::collections::HashMap;
+use thiserror::Error;
 
-use futures::{
-  stream::{self, BufferUnordered},
-  StreamExt,
-};
+use futures::{stream, StreamExt};
 use reqwest::{
-  header::{HeaderMap, HeaderValue},
-  Client,
+    header::{HeaderMap, HeaderValue},
+    Client,
 };
 
 pub struct VerifierClient {
-  project_slug: String,
-  client: Client,
+    project_slug: String,
+    client: Client,
 }
 
 impl VerifierClient {
-  pub fn new(project_slug: String, api_key: &str) -> Self {
-    let mut headers = HeaderMap::new();
-    let mut api_key_header_value = HeaderValue::from_str(api_key).unwrap();
-    api_key_header_value.set_sensitive(true);
-    headers.insert("Circle-Token", api_key_header_value);
-    let client = Client::builder().default_headers(headers).build().unwrap();
+    pub fn new(project_slug: String, api_key: &str) -> Self {
+        let mut headers = HeaderMap::new();
+        let mut api_key_header_value = HeaderValue::from_str(api_key).unwrap();
+        api_key_header_value.set_sensitive(true);
+        headers.insert("Circle-Token", api_key_header_value);
+        let client = Client::builder().default_headers(headers).build().unwrap();
 
-    Self {
-      client,
-      project_slug,
+        Self {
+            client,
+            project_slug,
+        }
     }
-  }
 
-  pub async fn get_job_artifacts(
-    &self,
-    job_number: &str,
-  ) -> Result<HashMap<String, String>, String> {
-    let json = self
-      .client
-      .get(format!(
-        "https://circleci.com/api/v2/project/{}/{}/artifacts",
-        &self.project_slug, job_number
-      ))
-      .send()
-      .await
-      .map_err(|e| e.to_string())?
-      .json::<serde_json::Value>()
-      .await
-      .map_err(|e| e.to_string())?;
+    pub async fn get_job_artifacts(
+        &self,
+        job_number: &str,
+    ) -> Result<HashMap<String, String>, String> {
+        let json = self
+            .client
+            .get(format!(
+                "https://circleci.com/api/v2/project/{}/{}/artifacts",
+                &self.project_slug, job_number
+            ))
+            .send()
+            .await
+            .map_err(|e| e.to_string())?
+            .json::<serde_json::Value>()
+            .await
+            .map_err(|e| e.to_string())?;
 
-    json
-      .as_object()
-      .and_then(|json_obj| json_obj.get("items"))
-      .and_then(|items_value| items_value.as_array())
-      .map(|items_arr| {
-        items_arr
-          .iter()
-          .flat_map(|item_value| {
-            item_value.as_object().and_then(|item_obj| {
-              let path = item_obj
-                .get("path")
-                .and_then(|path_value| path_value.as_str());
-              let url = item_obj.get("url").and_then(|url_value| url_value.as_str());
-              path
-                .zip(url)
-                .map(|(path, url)| (path.to_string(), url.to_string()))
+        json.as_object()
+            .and_then(|json_obj| json_obj.get("items"))
+            .and_then(|items_value| items_value.as_array())
+            .map(|items_arr| {
+                items_arr
+                    .iter()
+                    .flat_map(|item_value| {
+                        item_value.as_object().and_then(|item_obj| {
+                            let path = item_obj
+                                .get("path")
+                                .and_then(|path_value| path_value.as_str());
+                            let url = item_obj.get("url").and_then(|url_value| url_value.as_str());
+                            path.zip(url)
+                                .map(|(path, url)| (path.to_string(), url.to_string()))
+                        })
+                    })
+                    .collect::<HashMap<String, String>>()
             })
-          })
-          .collect::<HashMap<String, String>>()
-      })
-      .ok_or("Failed to parse JSON".to_string())
-  }
-
-  pub async fn assemble(
-    &self,
-    artifacts: HashMap<String, String>,
-  ) -> Result<VerificationMetadata, Vec<reqwest::Error>> {
-    let requests = stream::iter(vec![
-      "git/repo.txt",
-      "git/remote.txt",
-      "git/branch.txt",
-      "git/commit.txt",
-    ])
-    .map(|p| {
-      let client = &self.client;
-      let url = &artifacts[p];
-      async move { client.get(url).send().await?.text().await }
-    })
-    .buffer_unordered(2)
-    .collect::<Vec<Result<String, reqwest::Error>>>()
-    .await;
-
-    match requests[..] {
-      [Ok(ref repo), Ok(ref remote), Ok(ref branch), Ok(ref commit)] => {
-        Ok(VerificationMetadata {
-          repo: repo.trim().to_string(),
-          remote: remote.trim().to_string(),
-          branch: branch.trim().to_string(),
-          commit: commit.trim().to_string(),
-        })
-      }
-      _ => Err(requests.into_iter().filter_map(|r| r.err()).collect()),
+            .ok_or("Failed to parse JSON".to_string())
     }
-  }
+
+    pub async fn assemble(
+        &self,
+        artifacts: HashMap<String, String>,
+    ) -> Result<VerificationMetadata, reqwest::Error> {
+        let requests = stream::iter(vec![
+            "git/repo.txt",
+            "git/remote.txt",
+            "git/branch.txt",
+            "git/commit.txt",
+        ])
+        .map(|p| {
+            let client = &self.client;
+            let url = &artifacts[p];
+            async move { client.get(url).send().await?.text().await }
+        })
+        .buffer_unordered(2)
+        .collect::<Vec<Result<String, reqwest::Error>>>()
+        .await
+        .into_iter()
+        .collect::<Result<Vec<String>, reqwest::Error>>()?;
+
+        let code_download = self
+            .client
+            .get(&artifacts["out/out.wasm"])
+            .send()
+            .await?
+            .bytes()
+            .await?;
+
+        if let [repo, remote, branch, commit] = &requests[..] {
+            let code = code_download.as_ref().to_vec();
+            let code_hash = CodeHash::hash_bytes(&code);
+            Ok(VerificationMetadata {
+                repo: repo.trim().to_string(),
+                remote: remote.trim().to_string(),
+                branch: branch.trim().to_string(),
+                commit: commit.trim().to_string(),
+                code,
+                code_hash,
+            })
+        } else {
+            // Should be able to deconstruct the same number of items as were requested
+            unreachable!();
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum AssembleError {
+    #[error("Could not download metadata: {0:?}")]
+    MetadataDownload(#[from] reqwest::Error),
 }
 
 #[derive(Debug)]
 pub struct VerificationMetadata {
-  pub repo: String,
-  pub remote: String,
-  pub branch: String,
-  pub commit: String,
+    pub repo: String,
+    pub remote: String,
+    pub branch: String,
+    pub commit: String,
+    pub code: Vec<u8>,
+    pub code_hash: CodeHash,
 }
-
-// pub async fn get_job_artifacts(
-//   project_slug: &str,
-//   job_number: &str,
-//   api_key: &str,
-// ) -> Result<HashMap<String, String>, String> {
-//   let mut headers = HeaderMap::new();
-//   let mut api_key_header_value = HeaderValue::from_str(api_key).map_err(|e| e.to_string())?;
-//   api_key_header_value.set_sensitive(true);
-//   headers.insert("Circle-Token", api_key_header_value);
-//   let client = Client::builder()
-//     .default_headers(headers)
-//     .build()
-//     .map_err(|e| e.to_string())?;
-
-//   let json = client
-//     .get(format!(
-//       "https://circleci.com/api/v2/project/{project_slug}/{job_number}/artifacts"
-//     ))
-//     .send()
-//     .await
-//     .map_err(|e| e.to_string())?
-//     .json::<serde_json::Value>()
-//     .await
-//     .map_err(|e| e.to_string())?;
-
-//   json
-//     .as_object()
-//     .and_then(|json_obj| json_obj.get("items"))
-//     .and_then(|items_value| items_value.as_array())
-//     .map(|items_arr| {
-//       items_arr
-//         .iter()
-//         .flat_map(|item_value| {
-//           item_value.as_object().and_then(|item_obj| {
-//             let path = item_obj
-//               .get("path")
-//               .and_then(|path_value| path_value.as_str());
-//             let url = item_obj.get("url").and_then(|url_value| url_value.as_str());
-//             path
-//               .zip(url)
-//               .map(|(path, url)| (path.to_string(), url.to_string()))
-//           })
-//         })
-//         .collect::<HashMap<String, String>>()
-//     })
-//     .ok_or("Failed to parse JSON".to_string())
-// }
--- web-rs/src/main.rs
+++ web-rs/src/main.rs
@@ -6,49 +6,49 @@ use warp::Filter;
 mod circleci_artifacts;
 mod circleci_verify;
 mod env;
-use crate::{env::CIRCLECI_WEBHOOK_SECRET, circleci_artifacts::VerifierClient};
+use crate::{circleci_artifacts::VerifierClient, env::CIRCLECI_WEBHOOK_SECRET};
 
 #[tokio::main]
 async fn main() {
-  if let Err(_) = dotenv::dotenv() {
-    println!("No .env file found.");
-  }
+    if let Err(_) = dotenv::dotenv() {
+        println!("No .env file found.");
+    }
 
-  let project_slug = std::env::var(env::CIRCLECI_PROJECT_SLUG).unwrap();
-  let api_key = std::env::var(env::CIRCLECI_API_KEY).unwrap();
+    let project_slug = std::env::var(env::CIRCLECI_PROJECT_SLUG).unwrap();
+    let api_key = std::env::var(env::CIRCLECI_API_KEY).unwrap();
 
-  let vclient = VerifierClient::new(project_slug, &api_key);
-  let artifacts = vclient.get_job_artifacts("24").await.unwrap();
-  println!("{:?}", &artifacts);
-  let metadata = vclient.assemble(artifacts).await.unwrap();
-  println!("{:?}", &metadata);
+    let vclient = VerifierClient::new(project_slug, &api_key);
+    let artifacts = vclient.get_job_artifacts("24").await.unwrap();
+    println!("{:?}", &artifacts);
+    let metadata = vclient.assemble(artifacts).await.unwrap();
+    println!("{}", &metadata.code_hash);
 
-  // vclient.
+    // vclient.
 
-  // let artifacts = circleci_artifacts::get_job_artifacts(
-  //   &project_slug,
-  //   "24",
-  //   &api_key,
-  // )
-  // .await;
-  // println!("{:?}", artifacts);
+    // let artifacts = circleci_artifacts::get_job_artifacts(
+    //   &project_slug,
+    //   "24",
+    //   &api_key,
+    // )
+    // .await;
+    // println!("{:?}", artifacts);
 
-  return;
+    return;
 
-  let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
 
-  tracing_subscriber::fmt()
-    .with_env_filter(filter)
-    .with_span_events(FmtSpan::CLOSE)
-    .init();
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
 
-  let circleci_webhook_secret = var(CIRCLECI_WEBHOOK_SECRET).unwrap();
+    let circleci_webhook_secret = var(CIRCLECI_WEBHOOK_SECRET).unwrap();
 
-  let guarded = warp::path!("circle")
-    .and(circleci_verify::verify_filter(&circleci_webhook_secret))
-    .map(|| "ok without pass!");
+    let guarded = warp::path!("circle")
+        .and(circleci_verify::verify_filter(&circleci_webhook_secret))
+        .map(|| "ok without pass!");
 
-  let routes = guarded.with(warp::trace::request());
+    let routes = guarded.with(warp::trace::request());
 
-  warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
