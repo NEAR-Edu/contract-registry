caf5e2cc98766101d82b9e1c30b8d5f1d809eaf9
Jacob <encody@users.noreply.github.com>
2022-02-28 16:00:23 -0600

    correct data type for json args

--- contract/init-args.js
+++ contract/init-args.js
@@ -3,4 +3,7 @@ const ONE_DAY = 1_000_000_000 * 60 * 60 * 24;
 // 1e24, calculated like this because JS numbers don't work that large
 const ONE_NEAR = BigInt(1e12) ** 2n;
 
-console.log(JSON.stringify({}));
+console.log(JSON.stringify({
+    owner_id: "hatchet.testnet",
+    verification_fee: ONE_NEAR / 100n + '',
+}));
